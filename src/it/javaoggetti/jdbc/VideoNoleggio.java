c84859f51f82ce24ecb6748416c7a194e6405038
michele <michele@air-di-michele.homenet.telecomitalia.it>
2017-11-30 16:29:54 +0100

    Add Scanner

--- src/it/javaoggetti/jdbc/VideoNoleggio.java
+++ src/it/javaoggetti/jdbc/VideoNoleggio.java
@@ -1,6 +1,7 @@
 package it.javaoggetti.jdbc;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class VideoNoleggio {
 
@@ -15,7 +16,7 @@ public class VideoNoleggio {
 					//    (oggetto Connection)
 					
 					try {
-						Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/video_noleggio","root","root");
+						Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/video_noleggio","root","1716");
 
 //						PreparedStatement ps = c.prepareStatement("INSERT INTO genere(tipo) VALUES (?)");
 												
@@ -91,19 +92,56 @@ public class VideoNoleggio {
 						Statement s = c.createStatement();
 						
 						// 4. eseguire le query (e eventualmente ottenerne i risultati)
-
-						ResultSet rs = s.executeQuery("SELECT titolo FROM video_noleggio.film join genere on film.genere=genere.id where genere.tipo='DRAMMATICO' GROUP BY titolo;");
+						Scanner sc = new Scanner (System.in);
+						// TUTTO QUELLO CHE DOBBIAMO FAR FARE AL DB VIENE DOPO   statement s = c.createStatement();
 						
-						// 5. scorrere i risultati
+						System.out.println("\t \t \t \t \t Benvenuti \n" + " \t \t \t \t Video Noleggio GNOGNO!!!  \n \n \n");
+						System.out.println("Selezionare l'opzione desiderata: \n\n" + 
+						"OPZIONE 1" + " - - - " + "Controllare lista film: \n" + 
+						"OPZIONE 2" + " - - - " + "Controllare film noleggiati \n ");
 						
-						while(rs.next()) {
+						int scelta = sc.nextInt();
+						
+						System.out.println(scelta);
+						
+							if (scelta == 1) {
+								ResultSet rs1 = s.executeQuery(
+										"SELECT id, titolo FROM Film;"
+										);
+								while(rs1.next()) {									// fino a quando ho ancora cose da leggere
+												// stampa la riga corrente
+									for(int i = 1; i <= rs1.getMetaData().getColumnCount(); i++) {   // scandiscimi le colonne
+										System.out.println(rs1.getMetaData().getColumnLabel(i)+": "	// stampa il nome della colonna i
+													+rs1.getString(i) + " ");
+									}// stampa il contenuto della cella i
+								}
+							} else if (scelta == 2) {
+								ResultSet rs2 = s.executeQuery(
+										"SELECT id, titolo FROM Film "
+										+ "JOIN Noleggio ON Noleggio.film = Film.id "
+										+ "GROUP BY id, titolo; "
+										);
+								while(rs2.next()) {									// fino a quando ho ancora cose da leggere
+									// stampa la riga corrente
+									for(int i = 1; i <= rs2.getMetaData().getColumnCount(); i++) {   // scandiscimi le colonne
+										System.out.println(rs2.getMetaData().getColumnLabel(i)+": "	// stampa il nome della colonna i
+										+rs2.getString(i) + " ");
+									}
+								}
+							} else {
+								System.out.println("NESSUNA SCELTA SELEZIONATA \n SI PREGA DI SCEGLIERE LA GIUSTA OPZIONE!!");
+							}
+						
+						/*
+						  while(rs.next()) {
+						 
 							System.out.println("Row "+rs.getRow());
 							for(int i = 1; i < rs.getMetaData().getColumnCount()+1; i++) {
 								System.out.println(rs.getMetaData().getColumnLabel(i)+": "	// stampa il nome della colonna i
 											+rs.getString(i));								// stampa il contenuto della cella i
 							}
 						}
-						
+						*/
 					} catch (SQLException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -115,6 +153,8 @@ public class VideoNoleggio {
 					e.printStackTrace();
 				}
 				
+				System.out.println("Modifica stefano");
+						
 				
 	}
 
